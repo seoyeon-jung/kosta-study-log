fba333d97580946055a706b7fb8657cb678f33a7
seoyeon-jung <95006849+seoyeon-jung@users.noreply.github.com>
2024-07-03 15:44:26 +0900

    0703 api 사용 실습 TIL

---
+++ 4주차/240703/news_react/src/components/News.jsx
@@ -0,0 +1,76 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./News.css";
+
+export const API_KEY = "d4fdd4df1c96408290ab1366bf03c92e";
+
+const News = () => {
+  const category_arr = [
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology",
+  ];
+
+  const [newsList, setNewsList] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [category, setCategory] = useState("business");
+
+  const getNews = async () => {
+    try {
+      const res = await axios.get(
+        `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+      );
+      //console.log(res.data.articles);
+      const articles = res.data.articles;
+      setNewsList(articles);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCategory = (e) => {
+    //console.log(e.target.innerText);
+    setCategory(e.target.innerText);
+  };
+
+  useEffect(() => {
+    // category가 변경될 때마다 실행
+    getNews();
+  }, [category]);
+
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div className="news-list">
+      {category_arr.map((category, idx) => {
+        return (
+          <button key={idx} className="category-btn" onClick={handleCategory}>
+            {category}
+          </button>
+        );
+      })}
+      {newsList.map((article, idx) => {
+        return (
+          <div key={idx} className="news-item">
+            <div className="thumbnail">
+              <img src={article.urlToImage} alt={article.title} />
+            </div>
+            <div className="contents">
+              <h2>{article.title}</h2>
+              <p>{article.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default News;
